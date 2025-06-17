ff962d9bee6326664d42cf01e38de6c63a16ab65
rwbn9890 <rwbn1.alishan.as@gmail.com>
2025-06-17 16:50:29 +0530

    mongdb crud create delete

--- index.js
+++ index.js
@@ -1,27 +1,74 @@
 const express = require("express")
-const path = require("path")
-const db = require('./config/database')
-const admin = require("./models/adminTbl")
+const db = require("./config/db")
+const adminTbl = require("./model/admitTbl")
 
-let port = 9000;
+const port = 3300;
 
 const app = express();
 
 app.set("view engine", "ejs")
 
-app.use("/", express.static(path.join(__dirname, "/public")))
+app.use(express.urlencoded())
 
 
-app.get("/", (req, res)=> {
-    res.render('index')
+app.get("/delete/:id", (req, res)=>{
+    // let id = req.query.id
+    let id = req.params.id
+    adminTbl. findByIdAndDelete(id)
+     .then((data)=>{
+        console.log("data Deleted successfully...!")
+        res.redirect('/')
+        return false;
+    })
+    .catch((err)=>{
+        console.log(err)
+        return false;
+    })
 })
 
+app.post("/insertData", (req, res)=>{
+    console.log(req.body)
 
-app.listen(port, db, (err)=> {
-    if(err)
-    {
-        console.log("server is not connected...!")
+    const {name, email, phone, password} = req.body
+
+    adminTbl.create({
+        name,
+        email,
+        phone,
+        password
+    })
+    .then((data)=>{
+        console.log("data inserted successfully...!")
+        res.redirect('/')
+        return false;
+    })
+    .catch((err)=>{
+        console.log(err)
+        return false;
+    })
+
+})
+
+app.get("/", (req, res)=>{
+
+    adminTbl.find().then((allData)=>{
+    //     console.log(allData)
+       return res.render("index",{
+        data:allData
+       })
+        
+    })
+    .catch((err)=>{
+        console.log(err)
+        return false;
+    })
+
+})
+
+app.listen(port, (err)=>{
+    if(err){
+        console.log(err)
         return false;
     }
-    console.log("server is  connected to port ", + port)
+    console.log("server  is connected to " + port)
 })
